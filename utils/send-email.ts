ffb26c28e49933b4471807ec6d2d623bb2bf8278
affandindar <92578411+affandindar@users.noreply.github.com>
2024-07-09 18:23:51 +0300

    deleting send-email.ts

--- utils/send-email.ts
+++ utils/send-email.ts
@@ -1,17 +1,17 @@
-import { FormData } from '@/components/contact';
+// import { FormData } from '@/components/contact';
 
-export function sendEmail(data: FormData) {
-  const apiEndpoint = '/api/email';
+// export function sendEmail(data: FormData) {
+//   const apiEndpoint = '/api/email';
 
-  fetch(apiEndpoint, {
-    method: 'POST',
-    body: JSON.stringify(data),
-  })
-    .then((res) => res.json())
-    .then((response) => {
-      alert(response.message);
-    })
-    .catch((err) => {
-      alert(err);
-    });
-}
+//   fetch(apiEndpoint, {
+//     method: 'POST',
+//     body: JSON.stringify(data),
+//   })
+//     .then((res) => res.json())
+//     .then((response) => {
+//       alert(response.message);
+//     })
+//     .catch((err) => {
+//       alert(err);
+//     });
+// }
